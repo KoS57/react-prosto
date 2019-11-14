26084e4b6d28527c57fc4b2b40e7677f0d19d071
Kos <remark5@ukr.net>
2019-11-14 23:38:35 +0200

    users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './users.module.css'
 import userPhoto from '../../images/user.png'
 import { NavLink } from 'react-router-dom'
+import *as axios from 'axios'
 
 const Users = (props) => {
     let pageCount = Math.ceil(props.totalUserCount / props.pageSize);
@@ -18,7 +19,7 @@ const Users = (props) => {
         </div>
 
         {
-        props.user.map(u => <div key={u.id}>
+            props.user.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <NavLink to={'/profile/' + u.id}>
@@ -27,8 +28,35 @@ const Users = (props) => {
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                            ? <button onClick={() => {
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "1474af24-1eca-4f89-82d1-e9866a25514c"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.unfollow(u.id);
+                                        }
+                                    });
+                            }}>Unfollow</button>
+
+                            : <button onClick={() => {
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {},
+                                    {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY": "1474af24-1eca-4f89-82d1-e9866a25514c"
+                                        }
+                                    })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.follow(u.id);
+                                        }
+                                    });
+                            }}>
+                                Follow</button>}
 
                     </div>
                 </span>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,7 +9,8 @@ import Preloader from '../Preloader/Preloader'
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+        {withCredentials: true})
             .then(response => {
                 this.props.toggleIsFetching(false);   
                 this.props.setUsers(response.data.items);
@@ -19,7 +20,8 @@ class UsersContainer extends React.Component {
     onPAgeChange = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+    {withCredentials: true})
             .then(response => {
                 this.props.setUsers(response.data.items);
                 this.props.toggleIsFetching(false);
